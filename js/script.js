c629b3d15e788e6a9930dad8c4c73f196537001b
Dzis <Dzis99@yandex.ru>
2020-10-26 17:32:51 +0500

    Изменение структуры проекта

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.burger-menu').click(function(event) {
-        $('.burger-menu.,main-nav').toggleClass('active');
+        $('.burger-menu,.main-nav,.header-form').toggleClass('active');
         $('body').toggleClass('lock');
     });
  });
